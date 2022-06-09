3ebd65a6fc563a8d0cf20ae910a38a9526ddf6bd
Hemanth <105928667+kila-zoka@users.noreply.github.com>
2022-06-09 13:19:39 +0530

    matplotlib base and gray to bgr fail

--- Colorspace.py
+++ Colorspace.py
@@ -1,5 +1,7 @@
 from turtle import width
 import cv2 as cv
+from matplotlib.colors import rgb2hex
+import matplotlib.pyplot as plt
 
 pic = cv.imread('images/img.png')
 # cv.imshow('Original image', pic)
@@ -13,8 +15,28 @@ def rescale(frame, scale=0.2):
     return cv.resize(frame, dimension, interpolation=cv.INTER_AREA)
 
 
-resized_img = rescale(pic)
-cv.imshow('Resized image', resized_img)
-cv.imwrite('org_img.png', resized_img)
+img = rescale(pic)
+# cv.imshow('Resized image', img)
+# cv.imwrite('org_img.png', img)
+img2 = cv.imread('org_img.png')
+
+# BGR to Gray
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# gray to rgb
+
+bgr = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
+cv.imshow('GRAY --> BGR', bgr)
+
+# BGR to RGB
+
+rgb = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+
+# cv.imshow('rgb', rgb)
+
+plt.imshow(rgb)
+plt.show()
+
 
 cv.waitKey(0)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+img = rescale(pic)
